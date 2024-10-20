bb43a0936761b0569b609bc5214738cfeb60d68c
Livea Brito <159591382+LiveaBrito@users.noreply.github.com>
2024-10-20 03:36:58 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Fragmentos from './components/Fragmentos'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <Fragmentos/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Fragmentos.jsx
@@ -0,0 +1,12 @@
+function Fragmentos(){
+
+    return(
+        <>
+        <h3 class="h3">Olá caro leitor, prazer em te conhecer!</h3>
+        <p class="p">acho que ouvi passos atras de você... </p>
+
+        </>
+    )
+}
+
+export default Fragmentos
